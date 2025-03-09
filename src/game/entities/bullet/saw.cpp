285dacccb8b5beecf2ece7378f6a0c82e6ce47ce
QBaraki <buithanhquang2006@gmail.com>
2025-03-09 15:38:24 +0700

    add velocity to bullet/saw

--- src/game/entities/bullet/saw.cpp
+++ src/game/entities/bullet/saw.cpp
@@ -1,13 +1,21 @@
 #include "saw.h"
 
-Saw::Saw(SDL_Renderer* renderer, double start_time_, int w, int h, int x, int y) {
+Saw::Saw(SDL_Renderer* renderer, double start_time_, int w, int h, int x, int y, Vec2d velocity_) {
   LoadImage(renderer, "assets/bullets/saw.png");
   angle = 0.0;
+  position = Vec2d(x, y);
   SetRect(x, y, w, h);
+  velocity = velocity_;
 }
 
 void Saw::Update(float delta_time, float current_time) {
-  angle += 0.2 * delta_time;
+  angle -= 0.2 * delta_time;
+  if (angle < 0.0) {
+    angle += 360.0;
+  }
+  position += velocity * delta_time;
+  rect.x = static_cast<int>(position.x);
+  rect.y = static_cast<int>(position.y);
 }
 
 void Saw::Draw(SDL_Renderer* renderer) {
--- src/game/entities/bullet/saw.h
+++ src/game/entities/bullet/saw.h
@@ -2,12 +2,15 @@
 #define SAW_H
 
 #include "../level_object.h"
+#include "geometry/vec2d.h"
 
 class Saw : public LevelObject {
   float angle;
+  Vec2d position;
+  Vec2d velocity;
 
  public:
-  Saw(SDL_Renderer* renderer, double start_time_, int w, int h, int x, int y);
+  Saw(SDL_Renderer* renderer, double start_time_, int w, int h, int x, int y, Vec2d velocity_);
   void Update(float delta_time, [[maybe_unused]] float current_time);
   void Draw(SDL_Renderer* renderer);
 };
