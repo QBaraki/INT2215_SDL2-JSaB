d3107072626375296b7de75815166e0fd59144cf
QBaraki <btq06@pm.me>
2025-03-12 07:57:27 +0700

    upd: access modifier for renderer in MonoBehaviour

--- src/behaviours/mono_behaviour.h
+++ src/behaviours/mono_behaviour.h
@@ -1,7 +1,22 @@
 #ifndef MONO_BEHAVIOUR_H
 #define MONO_BEHAVIOUR_H
 
+#include <SDL2/SDL_h.>
+
 class MonoBehaviour {
+ protected:
+  const SDL_Renderer* renderer;
+
+ public:
+  MonoBehaviour(const SDL_Renderer* renderer_);
+  ~MonoBehaviour();
+
+  virtual void Update();
+  virtual void FixedUpdate();
+  virtual void Render();
+  virtual void EventHandler(SDL_event& event);
+  virtual void OnKeyUp(SDL_event& event);
+  virtual void OnKeyDown(SDL_event& event);
 };
 
 #endif  //!MONO_BEHAVIOUR_H
