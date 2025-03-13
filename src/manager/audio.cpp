76d7880ffaab99d51586b1e1297f78996a164af2
QBaraki <buithanhquang2006@gmail.com>
2025-03-13 11:54:19 +0700

    Add: FPS vars and audio manager

---
+++ src/manager/audio.cpp
@@ -0,0 +1,10 @@
+#include "audio.h"
+
+#include <iostream>
+#include <stdexcept>
+
+void audio::Init() {
+  if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
+    throw std::runtime_error("Failed to initialize SDL_mixer. Error: " + std::string(Mix_GetError()));
+  }
+}
---
+++ src/manager/audio.h
@@ -0,0 +1,12 @@
+#ifndef AUDIO_H
+#define AUDIO_H
+
+#include <SDL2/SDL_mixer.h>
+
+namespace audio {
+
+void Init();
+
+}  // namespace Audio
+
+#endif  // !AUDIO_H
