8a6ddf015ff9947ca129af7cd576c2a091e2cb28
QBaraki <buithanhquang2006@gmail.com>
2025-03-17 17:26:56 +0700

    Followup to previous commit, uncommented detection line

--- src/game/scenes/level.cpp
+++ src/game/scenes/level.cpp
@@ -65,9 +65,9 @@ void Level::Update() {
   // Update objects and check collision
   for (LevelObject* o : onscreen_objects) {
     o->Update();
-    //if (o->IsCollided(player)) {
-    //  std::cerr << "Level::Update(): Object with address " << o << " collided !\n ";
-    //}
+    if (o->IsCollided(player)) {
+      std::cerr << "Level::Update(): Object with address " << o << " collided !\n ";
+    }
   }
 }
 
