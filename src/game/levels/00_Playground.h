012c18626224f06731f28873bcfc9dc430cc8de7
QBaraki <buithanhquang2006@gmail.com>
2025-03-18 11:10:46 +0700

    changed the playground a bit

--- src/game/levels/00_Playground.h
+++ src/game/levels/00_Playground.h
@@ -13,12 +13,21 @@ namespace PlaygroundLevel {
 
 bool LoadLevel(SDL_Renderer* renderer, std::vector<LevelObject*>& object, Mix_Music*& music_player) {
   // Load objects
-  object.push_back(new Saw(renderer, 0.50, 50, 1280, 300, {-0.15, 0}));
-  object.push_back(new Saw(renderer, 1.50, 50, 1280, 500, {-0.15, 0}));
-  object.push_back(new Saw(renderer, 2.25, 50, 1280, 200, {-0.15, 0}));
-  object.push_back(new Saw(renderer, 4.00, 50, 1280, 200, {-0.15, 0}));
-  object.push_back(new Saw(renderer, 5.50, 50, 1280, 300, {-0.15, 0}));
-  object.push_back(new Saw(renderer, 6.00, 50, 1280, 350, {-0.15, 0}));
+  object.push_back(new Saw(renderer, 0.00, 50, 1260, 420, {-0.173, 0}));
+  object.push_back(new Saw(renderer, 0.66, 50, 1260, 420, {-0.173, 0}));
+  object.push_back(new Saw(renderer, 1.31, 50, 1260, 200, {-0.173, 0}));
+  object.push_back(new Saw(renderer, 1.75, 50, 1260, 420, {-0.173, 0}));
+  object.push_back(new Saw(renderer, 2.40, 50, 1260, 420, {-0.173, 0}));
+
+  object.push_back(new Saw(renderer, 3.48, 50, 1260, 420, {-0.173, 0}));
+  object.push_back(new Saw(renderer, 4.13, 50, 1260, 420, {-0.173, 0}));
+  object.push_back(new Saw(renderer, 4.78, 50, 1260, 200, {-0.173, 0}));
+  object.push_back(new Saw(renderer, 5.21, 50, 1260, 420, {-0.173, 0}));
+  object.push_back(new Saw(renderer, 5.86, 50, 1260, 420, {-0.173, 0}));
+  object.push_back(new Saw(renderer, 6.08, 50, 1260, 140, {-0.173, 0}));
+  object.push_back(new Saw(renderer, 6.40, 50, 1260, 350, {-0.173, 0}));
+  object.push_back(new Saw(renderer, 6.72, 50, 1260, 140, {-0.173, 0}));
+
 
   // Load music
   music_player = Mix_LoadMUS("assets/musics/00_playground/mus_corrupted_section_01.ogg");
