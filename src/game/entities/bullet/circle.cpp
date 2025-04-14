c39a322c71e5378913c942961ebbd10851a20a68
QBaraki <buithanhquang2006@gmail.com>
2025-04-14 09:33:45 +0700

    Simple entities/bullets/Circle constructor

--- src/game/entities/bullet/circle.cpp
+++ src/game/entities/bullet/circle.cpp
@@ -1,7 +1,63 @@
 #include "circle.h"
 
-#include "game/entities/level_object.h"
+#include "managers/time.h"
 
-struct Circle : public LevelObject {
+Circle::Circle(SDL_Renderer* renderer, double fatal_time_, double countdown_, double intact_time_, Vec2d midpoint_, double radius_) : LevelObject(renderer) {
+  fatal_time = fatal_time_;
+  countdown = original_countdown = countdown_;
+  intact_time = intact_time_;
+  radius = radius_;
+  midpoint = midpoint_;
+  angle = 0;
+  bubbing_time = disappearing_time = 0.07f;
+}
+
+void Circle::Update() {
+  countdown -= mTime::delta_time;
+  angle += 5;
+  if (angle >= 360) {
+    angle -= 360;
+  }
+  if (countdown > 0) {
+    return;
+  }
+  bubbing_time -= mTime::delta_time;
+  if (bubbing_time > 0.0f) {
+    // Do something when "appearing" here
+    ;
+    return;
+  } else {
+    intact_time -= mTime::delta_time;
+    if (intact_time <= 0.0f) {
+      disappearing_time -= mTime::delta_time;
+    } else {
+      // Do something when intact here
+      ;
+      return;
+    }
+  }
+  if (intact_time <= 0.0f && disappearing_time > 0.0f) {
+    // Do something when "disappearing" here
+    ;
+  }
+}
+
+void Circle::Render() {
   ;
-};
+}
+
+bool Circle::IsCollided(Player* target) {
+  return false;
+}
+
+bool Circle::IsOutOfScreen() {
+  return false;
+}
+
+bool Circle::IsDestroyed() {
+  return fatal_time <= 0.0f && disappearing_time <= 0.0f;
+}
+
+LevelObject* Circle::Clone() {
+  return new Circle(*this);
+}
--- src/game/entities/bullet/circle.h
+++ src/game/entities/bullet/circle.h
@@ -1,6 +1,25 @@
 #ifndef CIRCLE_H
 #define CIRCLE_H
 
+#include <SDL2/SDL.h>
 
+#include "game/entities/level_object.h"
+#include "geometry/vec2d.h"
+
+struct Circle : public LevelObject {
+  int size;
+  double angle, radius;
+  double countdown, original_countdown, fatal_time, intact_time, bubbing_time, disappearing_time;
+  Vec2d midpoint;
+
+ public:
+  Circle(SDL_Renderer* renderer, double fatal_time_, double countdown_, double intact_time_, Vec2d midpoint_, double radius_);
+  void Update() override;
+  void Render() override;
+  bool IsCollided(Player* target) override;
+  bool IsOutOfScreen() override;
+  bool IsDestroyed() override;
+  LevelObject* Clone() override;
+};
 
 #endif  // CIRCLE_H
